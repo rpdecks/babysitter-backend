30094f1d286cfeec12603916f68f1f855bcd47f4
rpdecks <rpdecks@gmail.com>
2020-06-30 23:41:52 -0400

    filters and sort work done

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,6 +9,9 @@ class Job < ApplicationRecord
   end
 
   def duration
-    distance_of_time_in_words(self.start_time, self.end_time)
+    # distance_of_time_in_words(self.start_time, self.end_time)
+    diff_in_seconds = self.end_time - self.start_time
+    hours = diff_in_seconds.to_f / 3600
+    return hours.to_f
   end
 end
